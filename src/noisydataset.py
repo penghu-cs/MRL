7d4e5f93e0a509723298e002e50e67c2ca8a2966
penghu-cs <45786915+penghu-cs@users.noreply.github.com>
2022-02-22 07:58:41 +0800

    Update noisydataset.py

--- src/noisydataset.py
+++ src/noisydataset.py
@@ -223,35 +223,3 @@ class cross_modal_dataset(data.Dataset):
 
     def __len__(self):
         return len(self.train_data[0])
-
-class PILRandomGaussianBlur(object):
-    """
-    Apply Gaussian Blur to the PIL image. Take the radius and probability of
-    application as the parameter.
-    This transform was used in SimCLR - https://arxiv.org/abs/2002.05709
-    """
-
-    def __init__(self, p=0.5, radius_min=0.1, radius_max=2.):
-        self.prob = p
-        self.radius_min = radius_min
-        self.radius_max = radius_max
-
-    def __call__(self, img):
-        do_it = np.random.rand() <= self.prob
-        if not do_it:
-            return img
-
-        return img.filter(
-            ImageFilter.GaussianBlur(
-                radius=random.uniform(self.radius_min, self.radius_max)
-            )
-        )
-
-
-def get_color_distortion(s=1.0):
-    # s is the strength of color distortion.
-    color_jitter = transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)
-    rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
-    rnd_gray = transforms.RandomGrayscale(p=0.2)
-    color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
-    return color_distort
